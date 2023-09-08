16a1012f97c4d5d3015b9adcc3c7cad6833124fd
Udayan-Singh <95754164+Udayan-Singh@users.noreply.github.com>
2023-09-08 18:39:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 // const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb+srv://udayans0107:udayans0107@cluster0.ec3en9l.mongodb.net/?retryWrites=true&w=majority' // Customer change url to your db you created in atlas
+const mongoURI = process.env.MONGO_URI // Customer change url to your db you created in atlas
 // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
